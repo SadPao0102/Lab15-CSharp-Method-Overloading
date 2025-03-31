5a1129e8858f7e17b01b2bc21d09520fda014416
SadPao0102 <thanyathep.hankla@gmail.com>
2025-03-31 22:43:18 +0700

    66030243_Thanyathep

--- Lab15_Ex01/obj/Debug/net8.0/Lab15_Ex01.AssemblyInfo.cs
+++ Lab15_Ex01/obj/Debug/net8.0/Lab15_Ex01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab15_Ex01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a3e5cedf8c921c32570742eca05a994d8eec99f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66192d0928ca8399208881e0b68d7143a0627f26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab15_Ex01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab15_Ex01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
